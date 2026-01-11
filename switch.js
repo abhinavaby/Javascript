d84dd2e2c4f6dbf0c305dafb3768e4a258e78594
abhinavaby <abhinavaby07@gmail.com>
2026-01-11 11:28:28 +0530

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,27 @@
+let day=window.prompt("enter day(1-7):")
+switch(day){
+    case "1":
+        window.alert("it is monday");
+        break;
+    case "2":
+        window.alert("it is tuesday");
+        break;
+    case "3":
+        window.alert("it is wednessday");
+        break;
+    case "4":
+        window.alert("it is thursday");
+        break;
+    case "5":
+        window.alert("it is friday");
+        break;
+    case "6":
+        window.alert("it is saturday");
+        break;
+    case "7":
+        window.alert("it is sunday");
+        break;
+    default:
+        window.alert("break")
+
+}
