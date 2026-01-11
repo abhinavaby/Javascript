65fbfc1618908d71690607668ac5e5cd6b2313dc
abhinavaby <abhinavaby07@gmail.com>
2026-01-11 11:28:50 +0530

    Add files via upload

---
+++ ternery.js
@@ -0,0 +1,5 @@
+let age=1
+age >= 18 ? window.alert("you are old") : window.alert("you are not old");
+let time=12;
+let greeting= time>=12 ? "good afternoon" : "good evening"
+console.log(greeting)
