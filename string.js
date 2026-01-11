67b3fecb6238491d44832ef611cc3d1573c7373f
abhinavaby <abhinavaby07@gmail.com>
2026-01-11 14:26:00 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,63 @@
+let username=".      abhinavaby.        ";
+let firstletter= username.charAt(0);
+let indexof = username.indexOf("v");
+let lastindexof = username.lastIndexOf("a")
+let length=username.length;
+
+console.log(firstletter);
+console.log(indexof);
+console.log(lastindexof);
+console.log(length);
+
+
+
+
+
+username=username.trim();
+console.log(username);
+
+
+username=username.toUpperCase();
+console.log(username);
+
+
+username=username.toLowerCase();
+console.log(username);
+
+
+
+username=username.repeat(3);
+console.log(username)
+
+
+
+username=username.startsWith("d");
+console.log(username);
+
+
+username="hel lo";
+name=username.endsWith("o");
+console.log(name);
+space=username.includes(" ")
+console.log(space);
+
+
+let phnumber="944-757-11-73";
+phnumber=phnumber.replaceAll("-","");
+console.log(phnumber);
+
+
+
+let phnumbers="944-757-11-73";
+phnumbers=phnumbers.padStart(16,"0");
+console.log(phnumbers);
+
+
+let phnumberss="944-757-11-73";
+phnumberss=phnumberss.padEnd(15,"0");
+console.log(phnumberss);
+
+
+
+
+
