56ea447fdd26d3a894ba75114f7f8cccbf17d857
abhinavaby <abhinavaby07@gmail.com>
2026-01-12 22:26:34 +0530

    Add files via upload

---
+++ forloop.js
@@ -0,0 +1,49 @@
+//print 1 to 5
+for(let i=1;i<=5;i++){
+    console.log(i);
+}
+console.log("");
+//print 5 to 0
+for(let i=5;i>=0;i--){
+    console.log(i);
+}
+console.log(`happy new year`);
+
+
+console.log("")
+
+//continue
+for(let i=0;i<=20;i++){
+    if(i==13){
+        continue;
+    }
+    else{
+        console.log(i);
+
+    }
+}
+
+
+console.log("")
+
+//break
+for(let i=0;i<=20;i++){
+    if(i==13){
+        break;
+    }
+    else{
+        console.log(i);
+    }
+   
+}
+
+//pass
+for(let i=0;i<=20;i++){
+    if(i==13){
+        pass;
+    }
+    else{
+        console.log(i);
+
+    }
+}
