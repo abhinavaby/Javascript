3ce4fd1ea98e543e0ab6f08dc76444886146dd85
abhinavaby <abhinavaby07@gmail.com>
2026-01-12 22:42:22 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,25 @@
+
+function happybirthday(name){
+    console.log(`happy birthday to you ${name}!`);
+    console.log("how are you? ");
+    console.log("who are you? ");
+    console.log("he he he");
+
+}
+
+
+//function call
+happybirthday("abhinav");
+
+console.log("")
+console.log("")
+
+
+function add(a,b){
+    return a+b;
+}
+
+let a=Number(window.prompt("first number: "));
+let b=Number(window.prompt("second number: "));
+let sum=add(a,b);
+window.alert(sum);
