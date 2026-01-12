e93875d4ef779c17321962ca69ce6ff0dd02668c
abhinavaby <abhinavaby07@gmail.com>
2026-01-12 21:21:30 +0530

    Add files via upload

---
+++ logical-operators.js
@@ -0,0 +1,30 @@
+//and=&&,or=||,not=!
+
+
+const temp=20;
+if(temp>0 && temp<=30){
+    console.log("the wether is good");
+}
+
+else{
+    console.log("the weather is bad");
+}
+
+if(temp<=0 || temp>30){
+    console.log("the wether is bad");
+}
+
+else{
+    console.log("the weather is good");
+}
+
+
+
+
+const issunny=0;
+if(issunny!=0){
+    console.log("its sunny");
+}
+else{
+    console.log("its not sunny");
+}
