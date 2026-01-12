89e41fb8262931bebcc46433c8cff5ebdd899537
abhinavaby <abhinavaby07@gmail.com>
2026-01-12 21:33:40 +0530

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,21 @@
+
+//= assignment operator
+//== comparison operator (compare if values are equal)
+//=== strict equality operator (compare if values & datatype are equal)
+//!= inequality operator
+// !== strict inequality operator
+
+
+
+const pi=3.14;
+if(pi=="3.14"){
+    console.log("its pi but the datatype  may or maynot be same ");
+
+}
+if(pi==="3.14"){
+    console.log("its pi and both datatypes are the same");
+}
+else{
+
+    console.log("both data types are't the same");
+}
