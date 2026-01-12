b0dfc72a520c20fcc5b73e728e0b18b57540a202
abhinavaby <abhinavaby07@gmail.com>
2026-01-12 20:42:49 +0530

    Add files via upload

---
+++ slicing.js
@@ -0,0 +1,10 @@
+const fullname="abhinav aby";
+let firstname=fullname.slice(0,7);
+let lastname=fullname.slice(8,)
+console.log(firstname,lastname)
+let firstletter=firstname[0];
+let lastchar=firstname[1];
+console.log(firstletter);
+console.log(lastchar);
+let firstnamee=fullname.slice(0,fullname.indexOf(" "))
+console.log(firstnamee)
