acc648a60db53e20c16c9a0b7d25d7fb1fd8e794
abhinavaby <abhinavaby07@gmail.com>
2026-01-21 19:50:53 +0530

    Add files via upload

---
+++ object.js
@@ -0,0 +1,17 @@
+const person1={
+    firstname:"abhinav",
+    lastname:"aby",
+    age:18,
+    isemployed:false,
+    sayhello:()=>console.log("hi am Abhinav Aby"),
+    eat:()=>console.log("I am eating food")
+
+}
+console.log(person1.firstname);
+console.log(person1.isemployed);
+console.log(person1.age);
+person1.sayhello();
+person1.eat();
+
+
+
